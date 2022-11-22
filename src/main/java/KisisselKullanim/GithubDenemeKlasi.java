b060f6710ca922606f3d68e8d995b9e980a53901
ilknuroz234 <astroceresim@gmail.com>
2022-11-22 22:15:49 +0100

    V2

--- src/main/java/KisisselKullanim/GithubDenemeKlasi.java
+++ src/main/java/KisisselKullanim/GithubDenemeKlasi.java
@@ -1,2 +1,7 @@
-package KisisselKullanim;public class GithubDenemeKlasi {
+package KisisselKullanim;
+
+public class GithubDenemeKlasi {
+    public static void main(String[] args) {
+        System.out.println("Hey Cavagit remote add origin https://github.com/ilknuroz234/githubDeneme.git");
+    }
 }
